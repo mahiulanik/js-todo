6b53e00775d2dff49669e92a89973028831d7eff
mahiulanik <mahiulanik@gmail.com>
2026-05-27 00:14:17 +0600

    Completed JS ToDo App

---
+++ script.js
@@ -0,0 +1,79 @@
+const inputBox = document.getElementById("input-box");
+const activeList = document.getElementById("active-list");
+const completedList = document.getElementById("completed-list");
+const activeCount = document.getElementById("active-count");
+const completedCount = document.getElementById("completed-count");
+
+function createTaskElement(taskText, isCompleted = false) {
+  let li = document.createElement("li");
+  li.textContent = taskText;
+  if (isCompleted) {
+    li.classList.add("checked");
+  }
+  let span = document.createElement("span");
+  span.innerHTML = "\u00d7";
+  li.appendChild(span);
+  return li;
+}
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write something!");
+    return;
+  }
+  let li = createTaskElement(inputBox.value);
+  activeList.appendChild(li);
+  inputBox.value = "";
+  saveData();
+  updateCounter();
+}
+
+inputBox.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
+
+function updateCounter() {
+  const activeTasks = activeList.querySelectorAll("li");
+
+  const completedTasks = completedList.querySelectorAll("li");
+
+  activeCount.innerHTML = activeTasks.length;
+
+  completedCount.innerHTML = completedTasks.length;
+}
+
+document.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    if (e.target.parentElement.id === "active-list") {
+      e.target.classList.add("checked");
+      completedList.appendChild(e.target);
+    } else {
+      e.target.classList.remove("checked");
+      activeList.appendChild(e.target);
+    }
+    saveData();
+    updateCounter();
+  } else if (
+    e.target.tagName === "SPAN" &&
+    e.target.parentElement.tagName === "LI"
+  ) {
+    e.target.parentElement.remove();
+    saveData();
+    updateCounter();
+  }
+});
+
+function saveData() {
+  localStorage.setItem("activeTasks", activeList.innerHTML);
+  localStorage.setItem("completedTasks", completedList.innerHTML);
+}
+
+function showTask() {
+  activeList.innerHTML = localStorage.getItem("activeTasks") || "";
+  completedList.innerHTML = localStorage.getItem("completedTasks") || "";
+}
+
+showTask();
+updateCounter();
